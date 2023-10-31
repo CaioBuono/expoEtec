01a7fc196baafdf2ee4466f2eaf85db559fb83e8
CAIO <apple@APPLEs-iMac.local>
2023-10-31 12:45:15 -0300

    Colocando favicon

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/img/centro-paula-souza-logo.svg" type="image/svg+xml">
     <link rel="stylesheet" href="/css/style.css">
     <title>Formulario de cadastro</title>
 </head>
